32868014921fc60a675b76127e2b1b6298a90177
hasinibalusu07 <hasini.balusu9@gmail.com>
2025-11-10 16:51:05 +0530

    Added SwapNumbers program

---
+++ SwapNumbers.java
@@ -0,0 +1,9 @@
+public class SwapNumbers{
+    public static void main(String[] args){
+        int a=10,b=5;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After Swapping : "+a+" and "+b);
+    }
+}
